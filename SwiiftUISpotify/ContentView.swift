a7e59f2aec672580c5a95308d523189b83dbf6f6
miadjalili <jalilimiad@gmail.com>
2024-04-09 20:32:50 -0700

    Initial Commit

---
+++ SwiiftUISpotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiiftUISpotify
+//
+//  Created by Miad Jalili on 2024-04-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiiftUISpotify/SwiiftUISpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiiftUISpotifyApp.swift
+//  SwiiftUISpotify
+//
+//  Created by Miad Jalili on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiiftUISpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
